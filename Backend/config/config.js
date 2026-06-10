792afd11cfdd43343a4a43eefddd04f4f76f302c
Deepak Kumar Soam <165383363+deepaksoam002@users.noreply.github.com>
2026-06-10 17:18:24 +0000

    write complete config.js

---
+++ Backend/config/config.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const Joi = require('joi');
+
+
+const schema = Joi.object().
+keys({
+    NODE_ENV : Joi.string().valid('production','devlopment','test').required(),
+    PORT : Joi.number().default(5000),
+    DB_USER :  Joi.string().required().description("PostgreSQL username!!"),
+    DB_PASSWORD : Joi.string().required().description("Postgre password"),
+    DB_HOST : Joi.string().required().description('PostgreSQL Host Name'),
+    DB_NAME : Joi.string().required().description('PostgreSQL name'),
+    DB_PORT : Joi.number().default(5432),
+    JWT_SECRET : Joi.string().required().description('JWT Secret Key')
+}).unknown();
+
+const {value : envVars, error} = schema.prefs({error : { lable : key}}).validate(process.env)
+
+if(error){
+    throw new Error(`Config Validation Error : $(error.message)`)
+};
+
+module.exports = {
+    env : envVars.NODE_ENV,
+    port : envVars.PORT,
+    db : {
+        db_user : envVars.DB_USER,
+        db_pass : envVars.DB_PASSWORD,
+        db_host : envVars.DB_HOST,
+        db_password : envVars.DB_PASSWORD,
+        db_port : envVars.DB_PORT
+    },
+    jwt : {
+       secret : envVars.JWT_SECRET
+
+    }
+}
