ca5f1ae11791e70ee4bf2cea5129885e1d13a3ba
Deepak Kumar Soam <165383363+deepaksoam002@users.noreply.github.com>
2026-06-16 17:06:08 +0000

    all login route created

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -5,6 +5,26 @@ async function handlePasswordHashing (password) {
     return hashedPassword;
 };
 
+async function handlePasswordComparison(password, hashed_Password){
+   try { 
+    const isPasswordValid = await bcrypt.compare(password, hashed_Password)
+    if(isPasswordValid){
+        return true;
+    }
+    else {
+        return false;
+    }
+   } catch (error) {
+    
+    console.error("internal error during password comparison : error");
+    throw new Error("An error occurred while while verifying credentials. ");
+   }
+    
+
+}
+
+
 module.exports = {
     handlePasswordHashing,
+    handlePasswordComparison
 }
---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const {handleNewUserRegistration,
+       handleEmailVerification,
+       handleEmailVerificationTokenValidation,
+       handleGoogleSignup,
+       handleUserLogin,
+       handleUserLogout,
+       handleForgotPassword,
+       handleResetPassword
+     } = require('../models/newUserModel');
+
+
+// Handle new user registration ---
+router.post('/signup', handleNewUserRegistration);
+
+// handle email verification ---
+router.post('/send-verification', handleEmailVerification); 
+
+// handle email verification token validation ---
+router.get('/verify-email/:token', handleEmailVerificationTokenValidation); 
+
+// Handle new user signup with Google ---
+router.get('/google', handleGoogleSignup); 
+
+// Handle user login ---
+router.post('/login', handleUserLogin); 
+
+// handle logout request ---
+router.delete('/logout', handleUserLogout); 
+
+// handle forget password request ---
+router.post('/forgot-password/:token', handleForgotPassword); 
+
+// handle reset password request ---
+router.post('/reset-password', handleResetPassword);
+
+
+
+module.exports = router;
